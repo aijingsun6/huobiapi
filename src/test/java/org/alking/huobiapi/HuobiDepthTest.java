0d9caae450d9237e2c13b60cfaec7daa9f0412fb
alking <aijingsun6@gmail.com>
2018-04-18 12:07:21 +0800

    depth rest test

--- src/test/java/org/alking/huobiapi/HuobiDepthTest.java
+++ src/test/java/org/alking/huobiapi/HuobiDepthTest.java
@@ -2,32 +2,36 @@ package org.alking.huobiapi;
 
 
 import org.alking.huobiapi.domain.HuobiOrderBook;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class HuobiDepthTest {
 
 
     @Test
-    public void test() throws InterruptedException {
-
+    public void wsTest() throws InterruptedException {
         HuobiApiClientFactory factory = HuobiApiClientFactory.newInstance();
         HuobiApiWSClient client = factory.newWSClient();
-
         HuobiOrderBook ob;
-
-        for (int i = 0 ; i < 100; i ++){
+        for (int i = 0 ; i < 60; i ++){
             try {
                 ob = client.depth("eosbtc", "step0");
                 System.out.println(ob);
-
-                // ob = client.depth("xrpbtc", "step0");
-                // System.out.println(ob);
 
             } catch (HuobiApiException e) {
 
             }
             Thread.sleep(1000);
         }
+    }
+
+    @Test
+    public void restTest() throws HuobiApiException {
+        HuobiApiClientFactory factory = HuobiApiClientFactory.newInstance();
+        HuobiApiRestClient client = factory.newRestClient();
+        HuobiOrderBook ob = client.depth("eosbtc", "step0");
+        Assert.assertNotNull( ob );
+        System.out.println(ob);
     }
 
 }
